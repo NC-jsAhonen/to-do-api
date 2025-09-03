f4ab98ca308e237097199659a0e99d47cd3e9018
Jukka Ahonen <jukka.samuel@gmail.com>
2025-09-03 14:03:29 +0300

    initialize data model

--- todo_api/todo/models.py
+++ todo_api/todo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class List(models.Model):
+    pass
+
+class Item(models.Model):
+    done = models.BooleanField()
+    text = models.CharField(max_length=200)
+    list = models.ForeignKey(
+        List,
+        on_delete=models.CASCADE,
+        related_name="items"
+    )
+
+    def toggle_done(self):
+        self.done = not self.done
